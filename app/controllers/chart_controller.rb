94f1d6b25aafd6556b8153fc5adc0a505410976d
Chia Chyuan Khor <ChiaKhor2012@u.northwestern.edu>
2012-05-14 16:29:47 -0500

    deploy README, sessions variable, reduced unused <div> tag

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -17,7 +17,10 @@ before_filter :bar_chart
 #  end
   
 def bar_chart
-  @uid = 'eDBqNUx1lc'
+  if session[:userID] == nil
+    session[:userID] =  'eDBqNUx1lc'
+  end
+  @uid = session[:userID]
   @TruckData = Trucks.where(:UserObjectID => @uid).all[0].SalesData
   @products = @TruckData.first #For initialization purposes only
   @prodName = {} #The keys are the products ID, the values are the product names
