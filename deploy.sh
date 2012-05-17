2d9beee2c26935cdc7f8f132ec229211fe86edbb
Jeanette Huang <jeanette.y.huang@gmail.com>
2012-05-17 11:54:46 -0500

    Changed deploy.sh to add automatic downloads of ruby, rails, etc. if the user does not currently have the correct versions installed

--- deploy.sh
+++ deploy.sh
@@ -8,32 +8,56 @@ echo
 pass=1
 ruby_v=`ruby -v | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
 if [ $ruby_v != 1.9.3 ]
-	then echo "Error: Ruby 1.9.3 is required, Type 'rvm install 1.9.3' to install."
-	pass=0
+	then echo "Error: Ruby 1.9.3 is required. Installing Ruby 1.9.3 ..."
+	rvm install 1.9.3
+	if [ $ruby_v != 1.9.3 ]
+		then echo "Ruby installation failed."
+		pass = 0
+	else
+		echo "Ruby 1.9.3 installation succeeded"
+	fi
 else
 	echo "Ruby version correct."
 fi
 
 rvm_v=`rvm -v | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
 if [ $rvm_v != 1.13.5 ]
-	then echo "Error: RVM 1.13.5 is required. Type 'curl -L get.rvm.io | bash -s stable' to install. "
-		pass=0
+	then echo "Error: RVM 1.13.5 is required. Installing RVM 1.13.5 ..."
+		curl -L get.rvm.io | bash -s stable
+		if [ $rvm_v != 1.13.5 ]
+			then echo "RVM installation failed."
+			pass=0
+		else
+			echo "RVM 1.13.5 installation succeeded."
+		fi
 else
 	echo "RVM version correct."
 fi
 
 rails_v=`rails -v | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
 if [ $rails_v != 3.2.3 ]
-	then echo "Error: Rails 3.2.3 is required. Type 'gem install rails' to install."
-		pass=0
+	then echo "Error: Rails 3.2.3 is required. Installing Rails 3.2.3 ..."
+		gem install rails
+		if [ $rails_v != 3.2.3 ]
+			then echo "Rails installation failed."
+			pass=0
+		else
+			echo "Rails 3.2.3 installation succeeded."
+		fi
 	else
 		echo "Rails version correct."
 fi
 
 bundler_v=`bundle -v | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
 if [ $bundler_v != 1.1.3 ]
-	then echo "Error: Bundler 1.1.3 is required. Type 'gem install bundler' to install."
-		pass=0
+	then echo "Error: Bundler 1.1.3 is required. Installing bundler 1.1.3 ..."
+		gem install bundler
+		if [ $bundler_v != 1.1.3 ]
+			then echo "Bundler installation failed."
+			pass=0
+		else
+			echo "Bundler 1.1.3 installation succeeded."
+		fi
 else
 	echo "Bundler version correct."
 fi
